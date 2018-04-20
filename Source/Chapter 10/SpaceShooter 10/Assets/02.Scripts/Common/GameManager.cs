c137e5f4e577a71f32b59b7ec2b45f7ff04e53c2
LeeJaeHyun <myleje@gmail.com>
2018-04-20 18:45:48 +0900

    chapter 10 upload
    미완성 버전

---
+++ Source/Chapter 10/SpaceShooter 10/Assets/02.Scripts/Common/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Enemy Create Info")]
+    //적 캐릭터가 출현할 위치를 담을 배열
+    public Transform[] points;
+    //적 캐릭터 프리팹을 저장할 변수
+    public GameObject enemy;
+    //적 캐릭터를 생성할 주기
+    public float createTime = 2.0f;
+    //적 캐릭터의 최대 생성 개수
+    public int maxEnemy = 10;
+    //게임 종료 여부를 판단할 변수
+    public bool isGameOver = false;
+
+    //싱글턴에 접근하기 위한 Static 변수 선언
+    public static GameManager instance = null;
+
+    [Header("Object Pool")]
+    //생성할 총알 프리팹
+    public GameObject bulletPrefab;
+    //오브젝트 풀에 생성할 개수
+    public int maxPool = 10;
+    public List<GameObject> bulletPool = new List<GameObject>();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        //instance에 할당된 클래스의 인스턴스가 다를 경우 새로 생성된 클래스를 의미함
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        //다른 씬으로 넘어가더라도 삭제하지 않고 유지함
+        DontDestroyOnLoad(this.gameObject);
+        //오브젝트 풀링 생성함수 호출
+        CreatePooling();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //하이러키 뷰의 SpawnPointGroup을 찾아 하위에 있는 모든 Transform 컴포넌트를 찾아옴
+        points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
+        if (points.Length > 0)
+        {
+            StartCoroutine(this.CreateEnemy());
+        }
+    }
+
+    //적 캐릭터를 생성하는 코루틴 함수
+    IEnumerator CreateEnemy()
+    {
+        //게임 종료 시까지 무한 루프
+        while (!isGameOver)
+        {
+            //현재 생성된 적 캐릭터의 개수 산출
+            int enemyCount = (int)GameObject.FindGameObjectsWithTag("ENEMY").Length;
+            //적 캐릭터의 최대 생성 개수보다 작을 때만 적 캐릭터를 생성
+            if (enemyCount < maxEnemy)
+            {
+                //적 캐릭터의 생성 주기 시간만큼 대기
+                yield return new WaitForSeconds(createTime);
+                //불규칙적인 위치 산출
+                int idx = Random.Range(1, points.Length);
+                //적 캐릭터의 동적 생성
+                Instantiate(enemy, points[idx].position, points[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    //오브젝트 풀에서 사용 가능한 총알을 가져오는 함수
+    public GameObject GetBullet()
+    {
+        for (int i = 0; i < bulletPool.Count; i++)
+        {
+            //비활성화 여부로 사용 가능한 총알인지를 판단
+            if (bulletPool[i].activeSelf == false)
+            {
+                return bulletPool[i];
+            }
+        }
+        return null;
+    }
+
+    //오브젝트 풀에 총알을 생성하는 함수
+    public void CreatePooling()
+    {
+        //총알을 생성해 차일드화할 페어런트 게임오브젝트를 생성
+        GameObject objectPools = new GameObject("ObjectPools");
+        //풀링 개수만큼 미리 총알을 생성
+        for (int i = 0; i < maxPool; i++)
+        {
+            var obj = Instantiate<GameObject>(bulletPrefab, objectPools.transform);
+            obj.name = "Bullet_" + i.ToString("00");
+            //비활성화 시킴
+            obj.SetActive(false);
+            //리스트에 생성한 총알 추가
+            bulletPool.Add(obj);
+        }
+    }
+}
---
+++ Source/Chapter 10/SpaceShooter 10/Assets/02.Scripts/Enemy/EnemyDamage.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyDamage : MonoBehaviour
+{
+    private const string bulletTag = "BULLET";
+    //생명 게이지
+    private float hp = 100.0f;
+    //초기 생명 수치
+    private float initHp = 100.0f;
+
+    //피격 시 사용할 혈흔 효과
+    private GameObject bloodEffect;
+    //EnemyAI 클래스에 접근하기 위한 변수
+    private EnemyAI enemyAI;
+
+    //생명 게이지 프리팹을 저장할 변수
+    public GameObject hpBarPrefab;
+    //생명 게이지의 위치를 보정할 오프셋
+    public Vector3 hpBarOffset = new Vector3(0, 2.2f, 0);
+    //부모가 될 Canvas 객체
+    private Canvas uiCanvas;
+    //생명 수치에 따라 fillAmount 속성을 변경할 Image
+    private Image hpBarImage;
+
+    void Start()
+    {
+        //EnemyAI 스크립트를 추출해 변수에 저장
+        enemyAI = GetComponent<EnemyAI>();
+
+        //혈흔 효과 프리팹을 로드
+        bloodEffect = Resources.Load<GameObject>("BulletImpactFleshBigEffect");
+
+        //생명 게이지의 생성 및 초기화
+        SetHpBar();
+    }
+
+    void SetHpBar()
+    {
+        uiCanvas = GameObject.Find("UI Canvas").GetComponent<Canvas>();
+        //UI Canvas 하위로 생명 게이지를 생성
+        GameObject hpBar = Instantiate<GameObject>(hpBarPrefab, uiCanvas.transform);
+        //fillAmount 속성을 변경할 Image를 추출
+        hpBarImage = hpBar.GetComponent<Image>();
+
+        //생명 게이지가 따라가야 할 대상과 오프셋 값 설정
+        var _hpBar = hpBar.GetComponent<EnemyHpbar>();
+        _hpBar.targetTr = this.gameObject.transform;
+        _hpBar.offset = hpBarOffset;
+    }
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == bulletTag)
+        {
+            //혈흔 효과를 생성하는 함수 호출
+            ShowBloodEffect(coll);
+            //총알 삭제
+            //Destroy(coll.gameObject);
+            coll.gameObject.SetActive(false);
+            //생명 게이지 차감
+            hp -= coll.gameObject.GetComponent<BulletCtrl>().damage;
+
+            //생명 게이지의 fillAmount 속성을 변경
+            hpBarImage.fillAmount = hp / initHp;
+
+            if (hp <= 0.0f)
+            {
+                //적 캐릭터의 상태를 DIE로 변경
+                GetComponent<EnemyAI>().state = EnemyAI.State.DIE;
+                //적 캐릭터가 사망한 이후 생명 게이지를 투명 처리
+                hpBarImage.GetComponentsInParent<Image>()[1].color = Color.clear;
+            }
+            else if (!enemyAI.isHit)
+            {
+                enemyAI.isHit = true;
+            }
+
+        }
+    }
+
+    //혈흔 효과를 생성하는 함수
+    void ShowBloodEffect(Collision coll)
+    {
+        //총알이 충돌한 지점 산출
+        Vector3 pos = coll.contacts[0].point;
+        //총알의 충돌했을 때의 법선 벡터
+        Vector3 _normal = coll.contacts[0].normal;
+        //총알의 충돌 시 방향 벡터의 회전값 계산
+        Quaternion rot = Quaternion.FromToRotation(-Vector3.forward, _normal);
+        //혈흔 효과 생성
+        GameObject blood = Instantiate<GameObject>(bloodEffect, pos, rot);
+        Destroy(blood, 1.0f);
+    }
+}
