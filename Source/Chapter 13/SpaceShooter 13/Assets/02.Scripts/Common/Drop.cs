cd8198b316a84ceee950510652dba94e9b95e8f7
LeeJaeHyun <myleje@gmail.com>
2018-04-22 23:15:44 +0900

    chapter 13 upload

---
+++ Source/Chapter 13/SpaceShooter 13/Assets/02.Scripts/Common/Drop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using DataInfo;
+
+public class Drop : MonoBehaviour , IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            Drag.draggingItem.transform.SetParent(this.transform);
+            //슬롯에 추가된 아이템을 GameData에 추가하기 위해 AddItem을 호출
+            Item item = Drag.draggingItem.GetComponent<ItemInfo>().itemData;
+            GameManager.instance.AddItem(item);
+        }
+    }
+}
